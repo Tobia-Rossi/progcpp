136c4a0f31bcb6fad604d8d3ee81018ed0fd9b91
Tobia Rossi <github@tobiarossi.ch>
2024-02-21 13:40:46 +0100

    Init Commit Initial repository commit with first hello world program on main branch

---
+++ prakt01/a1/hello.cpp
@@ -0,0 +1,33 @@
+//-------------------------------------------------------
+// Info
+//-------------------------------------------------------
+
+// Project Name:	Prakt01/a1
+// Project Code:	progpp01a1
+
+// File Name: 			hello.cpp
+// File Description: 	Main code implementation
+
+// Author Name:	Tobia Rossi
+// Author Code:	tuoyo
+
+//-------------------------------------------------------
+//Code Begin
+//-------------------------------------------------------
+
+// Application Include
+#include <iostream>
+
+// Namespace usage
+using namespace std;
+
+int main() {
+	cout << "Hello World!!" << endl;
+	
+	// Return No Error
+	return 0;
+}
+
+//-------------------------------------------------------
+//Code End
+//-------------------------------------------------------
